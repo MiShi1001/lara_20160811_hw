5e073ea9c11317892b922fc7115c40f3e4106a44
SU-RU <oooaa0727@gmail.com>
2017-10-13 11:16:22 +0800

    title name

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -79,13 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    最新消息
                 </div>
 
                 <div class="links">
                     <a href="{{route('home.index')}}">首頁</a>
                     <a href="{{route('about.index')}}">關於本站</a>
-                   
+
                 </div>
             </div>
         </div>
